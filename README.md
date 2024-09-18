99fbf783b7d9361e4041044065dc56e1267d3a36
Iris M <morais.iris@gmail.com>
2024-09-18 14:30:13 +0100

    Project 1 concentration game fixing function to work after there is a match, set constants for win function

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 setting up html
-getting modal to work
+getting modal to work. Modal pop up to appear on page load up.
+ouline main features on css
+getting timer function to start on page load up
+
+adding images 
+getting cards to flip on click. two cards should remain revealed after two clicks, cards to flip back if there is no match.
+count two clicks as a move, add it to the moves counter
+add timing and moves to the scoreboard at the end of the game
+display a win/lose message
+add cards shuffling at start of game and after new game
+
+get function to work after there is a match
+add timing and moves to scoreboard
+get buttons to work play, sound on/off, reset & back
+add timing and moves to the scoreboard at the end of the game
+display a win/loose message
--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
 /*-------------- Constants -------------*/
-
-
+const rules = {
+    moves: 50,
+    time: 150
+};
 /*---------- Variables (state) ---------*/
 let startTime = new Date().getTime();
-
+let cards = [];
+let firstCard, secondCard;
+let lockBoard = false;
+let moves = 0;
 /*----- Cached Element References  -----*/
-const timerTotal = document.querySelector("#timer-total")
-
+const timerTotal = document.querySelector("#timer-total");
+const gridContainer = document.querySelector(".grid-container");
+const movesTotal = document.querySelector("#moves-total");
+const modal = document.querySelector("#myModal");
+const openModal = document.querySelector("#openModal");
+const closeButton = document.querySelector(".close");
 /*-------------- Functions -------------*/
 setInterval(() => {
     const currentTime = new Date().getTime();
@@ -16,19 +25,105 @@ setInterval(() => {
     timerTotal.innerHTML = `${minutes} min : ${seconds} secs` ;
 }, 500)
 
+fetch("./data/cards.json")
+    .then((res) => res.json())
+    .then((data) => {
+        cards = [...data, ...data];
+        shuffleCards();
+        generateCards();
+    });
+
+function shuffleCards() {
+    let currentIndex = cards.length;
+    let randomIndex;    
+    let temporaryValue;
+    while (currentIndex !== 0) {   
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = cards[currentIndex];
+        cards[currentIndex] = cards[randomIndex];
+        cards[randomIndex] = temporaryValue;
+    }
+}
+
+function generateCards() {
+    for (let card of cards) {
+        const cardElement = document.createElement("div");
+        cardElement.classList.add("card");
+        cardElement.setAttribute("data-name", card.name);
+        cardElement.innerHTML = `
+            <div class="flip-card">
+                <div class="flip-card-inner">
+                    <div class="flip-card-front">
+                    </div>
+                    <div class="flip-card-back">
+                        <img class="flip-card-front-image" src=${card.image} />
+                    </div>
+                </div>
+            </div>
+        `;
+        gridContainer.appendChild(cardElement);
+        cardElement.addEventListener("click", flipCard);
+    }
+}
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.add("rotate");
+
+    if (!firstCard) {
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+    moves++;
+    movesTotal.innerHTML = moves;
+    lockBoard = true;
+
+    checkForMatch();
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+
+    resetBoard();
+}
+
+function unflipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove("rotate");
+        secondCard.classList.remove("rotate");
+        resetBoard();
+    }, 1000);
+}
+
+function resetBoard() {
+    firstCard = null;
+    secondCard = null;
+    lockBoard = false;
+}
+
+function restart() {
+    resetBoard();
+    shuffleCards();
+    moves = 0;
+    movesTotal.innerHTML = moves;
+    gridContainer.innerHTML = "";
+    generateCards();
+}
 /*----------- Event Listeners ----------*/
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
+openModal.onclick = function() {
     showModal();
 }
 
@@ -36,16 +131,16 @@ const showModal = function() {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+closeButton.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
+
+movesTotal.innerHTML = moves;
 
 showModal();
